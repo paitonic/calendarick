0593d5614e3ab453fec9d817523af961d8a64f1c
Vladimir Zeifman <edoomik@gmail.com>
2019-08-20 21:44:11 +0300

    Add test for leap years

--- src/calendar.test.js
+++ src/calendar.test.js
@@ -3,7 +3,11 @@ import { getCalendar, getDaysInMonth } from './calendar';
 describe('getDaysInMonth', () => {
   test('should return 31 for January 2019', () => {
     expect(getDaysInMonth(2019, 1)).toBe(31);
-  })
+  });
+
+  test('should work with leap years', () => {
+    expect(getDaysInMonth(2020, 2)).toBe(29);
+  });
 });
 
 describe('getCalendar', () => {
