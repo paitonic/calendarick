499133ce28977c37f287e8571822bd028aca6949
Vladimir Zeifman <edoomik@gmail.com>
2019-08-24 12:51:43 +0300

    Styling

--- examples/reactjs/index.js
+++ examples/reactjs/index.js
@@ -3,28 +3,82 @@ import React from 'react';
 
 import './calendar.sass';
 
-import { getCalendar } from '../../src/calendar';
+import { getCalendar, getWeekDays, groupByWeeks } from '../../src/calendar';
+import clsx from 'clsx';
 
 
-// function Week(props) {
-//   return ()
-// }
-//
 function Day(props) {
   return (
-    <div className="day">
+    <td className={clsx('day', {'day--empty': props.dayOfMonth === null})}>
       <span>{props.dayOfMonth}</span>
-    </div>
+    </td>
   )
 }
 
+function WeekDayNames(props) {
+  const dayNames = getWeekDays();
+  return (
+    <tr className='week-days'>
+      {
+        dayNames.map((dayName, index) => {
+          return (
+              <td key={`${dayName}-${index}`} className='day-name'>
+                {dayName}
+              </td>
+          )
+        })
+      }
+    </tr>
+  )
+}
+
+
+function Week(props) {
+  const dayNames = getWeekDays();
+  const weekWithPlaceholders = props.week.length === 7 ? props.week : [...new Array(7 - props.week.length).fill({dayOfMonth: null}), ...props.week];
+
+  return (
+    <tr className='week'>
+      {
+        weekWithPlaceholders.map((day, index) => {
+          return <Day key={index} weekDay={day.weekDay} dayOfMonth={day.dayOfMonth}/>
+        })
+      }
+    </tr>
+  )
+}
+
+function Month(props) {
+  const month = getCalendar(props.year, props.month);
+  const weeks = groupByWeeks(month);
+
+  return (
+    <table className='month'>
+      <tbody>
+        <WeekDayNames/>
+
+        {
+          weeks.map((week, index) => {
+            return <Week key={index} week={week}/>
+          })
+        }
+      </tbody>
+    </table>
+  )
+}
+
+/**
+
+ <WeekDayNames names={getWeekDays()}/>
+  {
+    getWeekDays(getCalendar(2019, 8)).map((days) => {
+      return <Week key={days[0].dayOfMonth} days={days}/>
+    })
+  }
+**/
 function App() {
   return (
-    <>
-      {getCalendar(2019, 8).map((day, index) => {
-        return <Day key={day.dayOfMonth} weekDay={day.weekDay} dayOfMonth={day.dayOfMonth}/>
-      })}
-    </>
+    <Month year={2019} month={8}/>
   );
 }
 
