d973a6b4b082b33eacece071b190d96fef361f32
Vladimir Zeifman <edoomik@gmail.com>
2019-10-21 07:15:02 +0300

    Add more e2e tests

--- examples/reactjs/cypress/integration/calendarik_spec.js
+++ examples/reactjs/cypress/integration/calendarik_spec.js
@@ -10,11 +10,11 @@ const defaultProps = {
   }
 };
 
-const visit = (url, props=defaultProps) => {
+const open = (variationName, props=defaultProps) => {
   if (props) {
-    cy.visit(`${url}?props=${encodeProps(props)}`);
+    cy.visit(`/${variationName}?props=${encodeProps(props)}`);
   } else {
-    cy.visit(url);
+    cy.visit('/' + variationName);
   }
 };
 
@@ -39,7 +39,24 @@ function format(date) {
   return `${year}-${month}-${day}`;
 }
 
-const tid = (testId) => `[data-testid="${testId}"]`;
+const tid = (testId) => {
+  const str = testId instanceof Date ? format(testId) : testId;
+  return `[data-testid="${str}"]`;
+};
+
+const today = new Date();
+
+const d_01 = fromArray([today.getFullYear(), today.getMonth()+1, 1]);
+const d_02 = fromArray([today.getFullYear(), today.getMonth()+1, 2]);
+const d_03 = fromArray([today.getFullYear(), today.getMonth()+1, 3]);
+const d_04 = fromArray([today.getFullYear(), today.getMonth()+1, 4]);
+const d_05 = fromArray([today.getFullYear(), today.getMonth()+1, 5]);
+
+const tid_01 = tid(d_01);
+const tid_02 = tid(d_02);
+const tid_03 = tid(d_03);
+const tid_04 = tid(d_04);
+const tid_05 = tid(d_05);
 
 describe('StaticDatePicker', () => {
   let today, today_year, today_month, today_day, todayTestId;
@@ -51,36 +68,36 @@ describe('StaticDatePicker', () => {
   });
 
   it('should display current month and year by default', () => {
-    visit('/StaticDatePicker', defaultProps);
+    open('StaticDatePicker', defaultProps);
     const monthName = today.toLocaleString(defaultProps.calendar.locale, {month: 'long'});
     cy.get(tid('month-' + today_month)).should('have.text', monthName);
     cy.get(tid('year-' + today_year)).should('have.text', String(today_year));
   });
 
   it('should change the style of the day when mouse is over', () => {
-    visit('/StaticDatePicker', defaultProps);
+    open('StaticDatePicker', defaultProps);
     // transparent
     cy.get(todayTestId).should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
 
     cy.get(todayTestId).trigger('mouseenter').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
   });
 
-  it('should not select any day', () => {
-    visit('/StaticDatePicker', defaultProps);
+  it('should no be selected days', () => {
+    open('StaticDatePicker', defaultProps);
     cy.get('.day--is-selected').should('not.exist')
   });
 
   it('should select date', () => {
-    visit('/StaticDatePicker', defaultProps);
+    open('StaticDatePicker', defaultProps);
     cy.get(todayTestId);
     cy.get(todayTestId).should('not.have.class', 'day--is-selected');
     cy.get(todayTestId).click().should('have.class', 'day--is-selected');
   });
 
-  it.skip('should show correct month and year when value props is given', () => {
+  it.skip('should show initially selected day', () => {
     const d_2020_01_01 = fromArray([2020, 1, 1]);
     const test_id_2020_01_01 = tid(format(d_2020_01_01));
-    visit('/StaticDatePickerWithValue', {...defaultProps, value: [ d_2020_01_01 ]});
+    open('StaticDatePicker', {...defaultProps, value: [ d_2020_01_01 ]});
 
     cy.get(test_id_2020_01_01).should('have.class', 'day--is-selected');
     cy.get(tid('month-1')).should('have.text', 'January');
@@ -88,7 +105,7 @@ describe('StaticDatePicker', () => {
   });
 
   it.skip('should not select disabled day', () => {
-    visit('/StaticDatePickerWithDisabledDays');
+    open('StaticDatePickerWithDisabledDays');
     const d_2020_01_02 = fromArray([2020, 1, 2]);
     const test_id_2020_01_02 = tid(d_2020_01_02);
 
@@ -101,7 +118,7 @@ describe('StaticDatePicker', () => {
   });
 
   it.skip('should hide days outside of month', () => {
-    visit('/StaticDatePickerWithValue', {
+    open('StaticDatePicker', {
       ...defaultProps,
       value: [ [fromArray([2020, 1, 1])] ],
       withOutsideDays: false
@@ -114,7 +131,7 @@ describe('StaticDatePicker', () => {
   });
 
   it.skip('should navigate month back when clicking on left arrow', () => {
-    visit('/StaticDatePickerWithValue', {...defaultProps, value: [ fromArray([2020, 1, 1]) ]});
+    open('StaticDatePicker', {...defaultProps, value: [ fromArray([2020, 1, 1]) ]});
 
     cy.get(tid('month-1')).should('have.text', 'January');
     cy.get(tid('year-2020')).should('have.text', '2020');
@@ -126,7 +143,7 @@ describe('StaticDatePicker', () => {
   });
 
   it.skip('should navigate month forward when clicking on right arrow', () => {
-    visit('/StaticDatePickerWithValue', {...defaultProps, value: [ fromArray([2020, 1, 1]) ]});
+    open('StaticDatePicker', {...defaultProps, value: [ fromArray([2020, 1, 1]) ]});
 
     cy.get(tid('month-1')).should('have.text', 'January');
     cy.get(tid('year-2020')).should('have.text', '2020');
@@ -138,7 +155,7 @@ describe('StaticDatePicker', () => {
   });
 
   it.skip('should navigate month back when clicking on right arrow (isRTL=true)', () => {
-    visit('/StaticDatePickerWithValue', {
+    open('StaticDatePicker', {
       ...defaultProps,
       value: [ fromArray([2020, 1, 1]) ],
       calendar: {...defaultProps.calendar, isRTL: true,}
@@ -154,7 +171,7 @@ describe('StaticDatePicker', () => {
   });
 
   it.skip('should navigate month forward when clicking on left arrow (isRTL=true)', () => {
-    visit('/StaticDatePickerWithValue', {
+    open('StaticDatePicker', {
       ...defaultProps,
       value: [fromArray([2020, 1, 1])],
       calendar: {...defaultProps.calendar, isRTL: true}
@@ -170,7 +187,7 @@ describe('StaticDatePicker', () => {
   });
 
   it.skip('should reverse order of week day names when isRTL=true (last day of week should be left-most)', () => {
-    visit('/StaticDatePickerWithValue', {
+    open('StaticDatePicker', {
       ...defaultProps,
       value: [fromArray([2020, 1, 1])],
       calendar: {...defaultProps.calendar, isRTL: true}
@@ -180,15 +197,120 @@ describe('StaticDatePicker', () => {
   });
 });
 
-describe('StaticDateRangePicker', function () {
-  beforeEach(() => {
-    visit('/StaticRangeDatePicker');
+describe('StaticDateRangePicker', () => {
+  it('should select range', () => {
+    open('StaticRangeDatePicker');
+    cy.get(tid_01).should('not.have.class', 'day--is-selected');
+    cy.get(tid_02).should('not.have.class', 'day--is-selected');
+    cy.get(tid_03).should('not.have.class', 'day--is-selected');
+
+    cy.get(tid_01).click().should('have.class', 'day--is-selected');
+    cy.get(tid_03).click().should('have.class', 'day--is-selected');
+
+    cy.get(tid_02).should('have.class', 'day--is-selected');
   });
 
-  it.skip('should select range', () => {
-    cy.get('[data-testid="2019-09-22"]').click().should('have.class', 'day--is-selected');
-    cy.get('[data-testid="2019-09-24"]').click().should('have.class', 'day--is-selected');
+  it('should show initially selected range', () => {
+    open('StaticRangeDatePicker', {
+      ...defaultProps,
+      value: [ [d_01, d_03] ]
+    });
 
-    cy.get('[data-testid="2019-09-23"]').should('have.class', 'day--is-selected');
+    cy.get(tid_01).should('have.class', 'day--is-selected');
+    cy.get(tid_02).should('have.class', 'day--is-selected');
+    cy.get(tid_03).should('have.class', 'day--is-selected');
+  });
+
+  it('should change initially selected range', () => {
+    open('StaticRangeDatePicker', {
+      ...defaultProps,
+      value: [ [d_01, d_03] ]
+    });
+
+    cy.get(tid_03).click().should('have.class', 'day--is-selected');
+    cy.get(tid_05).click().should('have.class', 'day--is-selected');
+
+    cy.get(tid_04).should('have.class', 'day--is-selected');
+
+    cy.get(tid_01).should('not.have.class', 'day--is-selected');
+    cy.get(tid_02).should('not.have.class', 'day--is-selected');
+  });
+
+  it('should not select disabled day', () => {
+    open('StaticRangeDatePickerWithDisabledDays');
+
+    cy.get(tid_01).click().should('have.class', 'day--is-selected');
+    cy.get(tid_03).click().should('not.have.class', 'day--is-selected');
+    cy.get(tid_02).should('not.have.class', 'day--is-selected');
+  });
+
+  it('should allow selecting range if there are disabled days in between', () => {
+    open('StaticRangeDatePickerWithDisabledDays');
+
+    cy.get(tid_01).click().should('have.class', 'day--is-selected');
+    cy.get(tid_04).click().should('have.class', 'day--is-selected');
+  });
+
+  it('should allow selecting late date before early date (e.g first 2020-01-03, then 2020-01-01)', () => {
+    open('StaticRangeDatePicker');
+
+    cy.get(tid_03).click().should('have.class', 'day--is-selected');
+    cy.get(tid_01).click().should('have.class', 'day--is-selected');
+
+    cy.get(tid_02).should('have.class', 'day--is-selected');
+  });
+
+  it('should select range of one day', () => {
+    open('StaticRangeDatePicker');
+
+    cy.get(tid_01).click().should('have.class', 'day--is-selected');
+    cy.get(tid_01).click().should('have.class', 'day--is-selected');
+
+    cy.get(tid_02).click().should('have.class', 'day--is-selected');
+    cy.get(tid_03).click().should('have.class', 'day--is-selected');
+
+    cy.get(tid_01).should('not.have.class', 'day--is-selected');
+  });
+});
+
+describe('StaticMultiSelectDatePicker', () => {
+  it('should select multiple days', () => {
+    open('StaticMultiSelectDatePicker');
+
+    cy.get(tid_01).click().should('have.class', 'day--is-selected');
+    cy.get(tid_03).click().should('have.class', 'day--is-selected');
+    cy.get(tid_02).should('not.have.class', 'day--is-selected');
+  });
+
+  it('should not select disable day', () => {
+    open('StaticMultiSelectDatePickerWithDisabledDays');
+
+    cy.get(tid_02).should('have.class', 'day--is-disabled');
+    cy.get(tid_02).should('not.have.class', 'day--is-selected');
+
+    cy.get(tid_02).click().should('have.class', 'day--is-disabled');
+    cy.get(tid_02).should('not.have.class', 'day--is-selected');
+  });
+
+  it('should show multiple initially selected days', () => {
+    open('StaticMultiSelectDatePicker', {...defaultProps, value: [ d_01, d_02 ]});
+
+    cy.get(tid_01).should('have.class', 'day--is-selected');
+    cy.get(tid_02).should('have.class', 'day--is-selected');
+  });
+
+  it('should deselect day', () => {
+    open('StaticMultiSelectDatePicker', {...defaultProps, value: [ d_01, d_02 ]});
+
+    cy.get(tid_01).click().should('not.have.class', 'day--is-selected');
+    cy.get(tid_02).should('have.class', 'day--is-selected');
+  });
+
+  it('should change selected days', () => {
+    open('StaticMultiSelectDatePicker', {...defaultProps, value: [ d_01, d_02 ]});
+
+    cy.get(tid_03).click().should('have.class', 'day--is-selected');
+    cy.get(tid_01).should('have.class', 'day--is-selected');
+    cy.get(tid_02).should('have.class', 'day--is-selected');
   });
 });
--- examples/reactjs/variations/Variations.js
+++ examples/reactjs/variations/Variations.js
@@ -2,18 +2,14 @@ import React, { useState } from 'react';
 import { Calendarik, DatePickerWithPopup } from '../DevelopmentPage';
 import { fromArray, isSame, nextDayOf } from '../../../src/calendar';
 
+const today = new Date();
+const d_02 = fromArray([today.getFullYear(), today.getMonth()+1, 2]);
+const d_03 = fromArray([today.getFullYear(), today.getMonth()+1, 3]);
 
-export const StaticDatePicker = (props) => {
-  return (
-    <Calendarik {...{selectionMode: 'single', ...props}}/>
-  )
-};
+export const StaticDatePicker = (props) => <Calendarik {...{selectionMode: 'single', ...props}}/>;
 
-export const StaticDatePickerWithValue = (props) => {
-  return (
-    <Calendarik {...{selectionMode: 'single', ...props}} value={props.value}/>
-  )
-};
+export const StaticDatePickerWithValue = (props) =>
+  <Calendarik {...{selectionMode: 'single', ...props}} value={props.value}/>;
 
 export const StaticDatePickerWithDisabledDays = (props) => {
   const d_2020_01_01 = fromArray([2020, 1, 1]);
@@ -26,15 +22,23 @@ export const StaticDatePickerWithDisabledDays = (props) => {
   )
 };
 
-export const StaticRangeDatePicker = (props) => {
+export const StaticRangeDatePicker = (props) => <Calendarik {...{selectionMode: 'range', ...props}}/>;
+
+export const StaticRangeDatePickerWithDisabledDays = (props) => {
+  const disableDays = (day) => [d_02, d_03].some(date => isSame(day, date));
+
   return (
-    <Calendarik {...{selectionMode: 'range', ...props}}/>
-  );
+    <Calendarik {...{selectionMode: 'range', disableDays: disableDays, ...props}}/>
+  )
 };
 
-export const StaticMultiSelectDatePicker = (props) => {
+export const StaticMultiSelectDatePicker = (props) => <Calendarik {...{selectionMode: 'multiple', ...props}}/>;
+
+export const StaticMultiSelectDatePickerWithDisabledDays = (props) => {
+  const disableDays = (day) => [d_02, d_03].some(date => isSame(day, date));
+
   return (
-    <Calendarik {...{selectionMode: 'multiple', ...props}}/>
+    <Calendarik {...{selectionMode: 'multiple', disableDays: disableDays, ...props}}/>
   )
 };
 
